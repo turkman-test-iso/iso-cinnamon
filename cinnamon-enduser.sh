8be48dcd1bf0a16f37286048dc9e6c5b4134dc9e
03tekno <162546868+03tekno@users.noreply.github.com>
2024-08-21 10:02:39 +0300

    Update cinnamon-enduser.sh

--- cinnamon-enduser.sh
+++ cinnamon-enduser.sh
@@ -21,10 +21,10 @@ sed -i "s|#agetty_options.*|agetty_options=\" -l /usr/bin/login\"|" /etc/conf.d/
 chmod u+s /bin/su /usr/bin/su
 # set language
 mkdir -p /lib64/locale
-echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
+echo "tr_TR.UTF-8 UTF-8" > /etc/locale.gen
 echo "" >> /etc/locale.gen
-echo "export LANG=en_US.UTF-8" > /etc/profile.d/locale.sh
-echo "export LC_ALL=en_US.UTF-8" >> /etc/profile.d/locale.sh
+echo "export LANG=tr_TR.UTF-8" > /etc/profile.d/locale.sh
+echo "export LC_ALL=tr_TR.UTF-8" >> /etc/profile.d/locale.sh
 locale-gen
 # polkit enable
 chmod u+s /usr/bin/pkexec /usr/lib64/polkit-1/polkit-agent-helper-1
