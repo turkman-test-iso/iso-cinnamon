d639f84933c305d3ccfb09e502c3533ea2983e89
mobilturka <162546868+03tekno@users.noreply.github.com>
2024-09-07 21:50:08 +0300

    Update cinnamon-enduser.sh

--- cinnamon-enduser.sh
+++ cinnamon-enduser.sh
@@ -9,7 +9,7 @@ chmod 755 /sbin/init
 # install cinnamon
 ymp repo --update --allow-oem --ignore-gpg
 ymp it xinit xorg-server xterm freetype xauth xkbcomp xkeyboard-config @x11.drivers --no-emerge --allow-oem
-ymp it elogind shadow pipewire wireplumber libtool firefox-installer mousepad inxi tzdata fuse fuse2 --no-emerge --allow-oem
+ymp it elogind shadow pipewire wireplumber libtool firefox-installer mousepad inxi tzdata ristretto fuse fuse2 --no-emerge --allow-oem
 ymp it @cinnamon gnome-screenshot caribou dejavu adwaita-icon-theme gsettings-desktop-schemas polkit-gnome gnome-terminal libhandy seatd touchegg --no-emerge --allow-oem
 ymp it gnome-icon-theme gnome-themes-standard lightdm lightdm-gtk-greeter --no-emerge --allow-oem
 
